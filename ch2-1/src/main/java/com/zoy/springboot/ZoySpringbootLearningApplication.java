01f8fea511c73ec65a2585f6d4ad3f0b4bf3f05b
zoy2control <paulino_zoy_it@163.com>
2019-02-01 14:35:23 +0800

    构建一个较为复杂的RESTful API以及单元测试 v0.1

---
+++ ch2-1/src/main/java/com/zoy/springboot/ZoySpringbootLearningApplication.java
@@ -0,0 +1,14 @@
+package com.zoy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoySpringbootLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoySpringbootLearningApplication.class, args);
+	}
+
+}
+
