05f736eee6ddf5cb9df836e531985ee676fa1998
zoy2control <paulino_zoy_it@163.com>
2019-02-15 11:23:09 +0800

    线程池--Future v0.1

--- ch5-2/src/test/java/com/zoy/springboot/service/util/TaskTest.java
+++ ch5-2/src/test/java/com/zoy/springboot/service/util/TaskTest.java
@@ -2,12 +2,14 @@ package com.zoy.springboot.service.util;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by zouzp on 2019/2/14.
@@ -17,9 +19,15 @@ import java.util.concurrent.Future;
 @EnableAsync
 public class TaskTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(TaskTest.class);
+
     @Autowired
     private Task task;
 
+    /**
+     * ·测试 任务之间的同步、异步
+     * @throws Exception
+     */
     @Test
     public void test() throws Exception{
         long startTime = System.currentTimeMillis();
@@ -34,5 +42,16 @@ public class TaskTest {
         }
         long endTime = System.currentTimeMillis();
         System.out.println("总共耗时 ： " + (endTime - startTime));
+    }
+
+    /**
+     * ·测试Future
+     * @throws Exception
+     */
+    @Test
+    public void testFuture() throws Exception {
+        Future<String> taskOneFuture = task.doTaskOne();
+        String result = taskOneFuture.get(5, TimeUnit.SECONDS);
+        logger.info(result);
     }
 }
