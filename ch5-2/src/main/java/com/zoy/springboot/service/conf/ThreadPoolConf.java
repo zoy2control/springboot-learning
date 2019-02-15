4b4f20103c7dcf36d772774f0e8a0bdecda9f488
zoy2control <paulino_zoy_it@163.com>
2019-02-15 10:17:28 +0800

    x线程池--优雅关闭 v0.2

--- ch5-2/src/main/java/com/zoy/springboot/service/conf/ThreadPoolConf.java
+++ ch5-2/src/main/java/com/zoy/springboot/service/conf/ThreadPoolConf.java
@@ -27,6 +27,7 @@ public class ThreadPoolConf {
         // ·线程池对拒绝任务的处理策略：这里采用了CallerRunsPolicy策略，当线程池没有处理能力的时候，
         // ·该策略会直接在 execute 方法的调用线程中运行被拒绝的任务；如果执行程序已关闭，则会丢弃该任务
         taskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        taskExecutor.setWaitForTasksToCompleteOnShutdown(true);// ·优雅关闭
 
         return taskExecutor;
     }
